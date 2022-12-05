78e877abe753b54a48eaf03fc0cbe87b3dd2f39a
biruk kalewold <birukkalewoldasrat@gamil.com>
2022-12-05 13:24:55 +0300

    added functionality to close side drawer when a linked on it is clicked

--- js/main.js
+++ js/main.js
@@ -3,13 +3,25 @@ const colseHumbergerIconElement = document.querySelector(
   "#mobile-nav .humburger"
 );
 const mobilelNav = document.querySelector("#mobile-nav");
+const backdropElement = document.querySelector(".backdrop");
+const mobileNavItemLInkElements =
+  document.querySelectorAll(".mobile-nav-item a");
 
 openHumburgerIconElement.addEventListener("click", openSideDrawer);
+colseHumbergerIconElement.addEventListener("click", closeSideDrawer);
+backdropElement.addEventListener("click", closeSideDrawer);
+
+for (const mobileNavItemLink of mobileNavItemLInkElements) {
+  mobileNavItemLink.addEventListener("click", closeSideDrawer);
+}
+
 function openSideDrawer() {
   mobilelNav.style.display = "block";
+  backdropElement.style.display = "block";
 }
 
 colseHumbergerIconElement.addEventListener("click", closeSideDrawer);
 function closeSideDrawer() {
   mobilelNav.style.display = "none";
+  backdropElement.style.display = "none";
 }
